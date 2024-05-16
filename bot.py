f58016eecca5669d8d733e8419d6c9c398be5eec
Eta <pleasefckoff00@gmail.com>
2024-05-16 22:13:30 +0100

    QoL Updates + Extra commands
    Added Syncing to a command and not on_ready
    Added Moderator system.
    Added BotHandler and a Useless command

--- bot.py
+++ bot.py
@@ -4,11 +4,14 @@ from discord.ext import commands
 import json
 from os import path
 import main
+import BotHandler
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 
 There are a number of utility commands being showcased here.'''
+
+owner_id = 1053570332339998801
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,11 +23,25 @@ bot = commands.Bot(command_prefix='Zeta', description=description, intents=inten
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
+
+    BotHandler.Mod(owner_id)
+
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+
+    print(ctx.author.id)
+    if BotHandler.isMod(ctx.author.id):
+        if ctx.content == "$sync":
+            try:
+                synced = await bot.tree.sync()
+                print(f"Synced {len(synced)} command(s)")
+            except Exception as e:
+                print(e) 
+
+    return
+    
 
 @bot.tree.command(
     name="message",
@@ -33,16 +50,21 @@ async def on_ready():
 @app_commands.describe(msg = "Message you want to say!")
 async def message(interaction: discord.Interaction, msg: str):
     ## interaction.message.author.id
+    channel_id = interaction.channel
+    user = interaction.user
 
-    response = main.ChatMessage(msg, 'user', interaction.user.id)
-    await interaction.response.send_message(response)
+    response = await main.ChatMessage(msg, 'user', interaction.user.id)
+    try:
+        await interaction.response.send_message(response)
+    except Exception as e:
+        await channel_id.send(user.mention + "\n" + response)
 
 @bot.tree.command(
     name = "createhistory",
     description= "Creates history with Zeta!"
 )
 async def createhistory(interaction: discord.Interaction):
-    await main.CreateHistory(interaction.user.id)
+    main.CreateHistory(interaction.user.id)
     await interaction.response.send_message("Created History!")
 
 @bot.tree.command(
@@ -54,59 +76,35 @@ async def clearhistory(interaction: discord.Interaction):
     await interaction.response.send_message("Cleared History!")
 
 @bot.tree.command(
-    name="download",
-    description="Download a history file!"
+    name = "upload",
+    description="[MODERATOR ONLY] Uploads All Data to the Moderation website"
 )
-@app_commands.describe(historyid = "History you want to download!")
-async def download(interaction: discord.Interaction, historyid: int):
-    ## interaction.message.author.id
-    channel_id = interaction.channel
+async def upload(interaction: discord.Interaction):
+    if BotHandler.isMod():
+        main.UploadData(interaction.user.id)
+        await interaction.response.send_message("Uploaded Data")
 
-    response = await main.DownloadHistory(historyid)
+@bot.tree.command(
+    name = "mod",
+    description="[OWNER] Mods a User"
+)
+@app_commands.describe(userid = "User you want to mod")
+async def mod(interaction: discord.Interaction, userid: str):
 
-    if response != None:
-        await channel_id.send(file=discord.file(response))
-    else:
-        await channel_id.send("No history avaliable")
+    if interaction.user.id == owner_id:
+        BotHandler.Mod(userid)
+        await interaction.response.send_message("Modded User")
+    return
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@bot.tree.command(
+    name = "unmod",
+    description="[OWNER] UnMods a User"
+)
+@app_commands.describe(userid = "User you want to unmod")
+async def unmod(interaction: discord.Interaction, userid: str):
+    if interaction.user.id == owner_id:
+        BotHandler.UnMod(userid)
+        await interaction.response.send_message("UnModded User")
+    return
 
 bot.run('TOKEN')
